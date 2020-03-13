5bc15a35a0178f57fb8dfa26a155a9d65b1d5d87
Nemanja Kuzmić <kuzmicnemanja@live.com>
2020-03-13 10:14:09 +0100

    Written a simple client application that sends message to mailslot.

---
+++ SimpleTest/main.cpp
@@ -0,0 +1,27 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	HANDLE h = CreateFile(
+		L"\\\\.\\mailslot\MyMailBox",
+		GENERIC_WRITE,
+		FILE_SHARE_READ,
+		nullptr,
+		OPEN_EXISTING,
+		0,
+		nullptr);
+
+	if (h == INVALID_HANDLE_VALUE) {
+		printf("Error: %d\n", GetLastError());
+		return 1;
+	}
+
+	WCHAR text[] = L"Test client says hi!";
+	DWORD writtenBytes;
+
+	WriteFile(h, text, sizeof(text), &writtenBytes, nullptr);
+	CloseHandle(h);
+
+	return 0;
+}
