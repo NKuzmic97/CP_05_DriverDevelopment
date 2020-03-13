20652a960c75b1a947df49945247bddb45dc7cda
Nemanja Kuzmić <kuzmicnemanja@live.com>
2020-03-13 15:57:23 +0100

    Completed and tested simple Win32AsyncIO application that loads 1GB file into RAM asynchronously. For Windows, use next cmd command to create empty file: "fsutil file createnew e:\test.dat 1073741824".

--- Win32AsyncIO/main.cpp
+++ Win32AsyncIO/main.cpp
@@ -3,7 +3,48 @@
 #include <tchar.h>
 
 int _tmain(int argc, _TCHAR* argv[]) {
-	std::cout << "Hello world!" << std::endl;
+	HANDLE hFile = CreateFile(
+		L"e:\\test.dat",
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		nullptr,
+		OPEN_EXISTING,
+		FILE_FLAG_OVERLAPPED,
+		nullptr
+	);
+
+	if (hFile == INVALID_HANDLE_VALUE) {
+		printf("Error: %d\n", GetLastError());
+		return 1;
+	}
+
+	DWORD size = GetFileSize(hFile, nullptr);
+
+	void* buffer = VirtualAlloc(nullptr, size, MEM_COMMIT, PAGE_READWRITE);
+	DWORD dummy;
+	OVERLAPPED ov = { 0 };
+	ov.hEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+
+	ReadFile(hFile, buffer, size, &dummy, &ov);
+
+	while (true) {
+		if (WaitForSingleObject(ov.hEvent, 100) == WAIT_OBJECT_0)
+			break;
+
+			printf(".");
+	}
+
+	printf("Done!\n");
+
+	GetOverlappedResult(hFile, &ov, &dummy, FALSE);
+
+	printf("Read: %d bytes.\n", dummy);
+
+	CloseHandle(hFile);
+
 	std::cin.get();
+
+	VirtualFree(buffer, 0, MEM_DECOMMIT);
+
 	return 0;
 }
